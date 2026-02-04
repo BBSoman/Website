dad4c36ec0581ff10397e2b3457e323bde8aadd0
BBSoman <harshumeshshah23@hotmail.com>
2026-02-04 15:35:12 +0400

    Update PartnersPage.tsx

--- src/pages/PartnersPage.tsx
+++ src/pages/PartnersPage.tsx
@@ -374,7 +374,7 @@ const PartnersPage: React.FC = () => {
         {
 
       logo: 'mago2.png', // Add the Mago logo image to your project
-      logoSize: 'h-12 w-30',
+      logoSize: 'h-14 w-30',
       description:
         'AI-powered real-time translation and communication solutions for seamless multilingual interactions.',
       keyServices: [
