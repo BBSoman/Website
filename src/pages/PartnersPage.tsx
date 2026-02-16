58059ef06c6af7c9a867b823c5b6b994c1bb313a
BBSoman <harshumeshshah23@hotmail.com>
2026-02-16 13:57:56 +0400

    Update PartnersPage.tsx

--- src/pages/PartnersPage.tsx
+++ src/pages/PartnersPage.tsx
@@ -416,7 +416,7 @@ const PartnersPage: React.FC = () => {
     },
     {
       logo: 'weblib.png', // Add the Weblib logo image to your project
-      logoSize: 'h-13 w-15',
+      logoSize: 'h-12 w-15',
       description:
         'Innovative web library solutions for seamless digital experiences and content management.',
       keyServices: [
